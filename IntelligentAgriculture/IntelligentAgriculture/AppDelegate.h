c60e98c506138c9fb888a5197635bed5796424af
YangFan <YF@>
2016-11-04 13:34:31 +0800

    初始化工程

---
+++ IntelligentAgriculture/IntelligentAgriculture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntelligentAgriculture
+//
+//  Created by YangFan on 16/11/4.
+//  Copyright © 2016年 YangFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntelligentAgriculture/IntelligentAgriculture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IntelligentAgriculture
+//
+//  Created by YangFan on 16/11/4.
+//  Copyright © 2016年 YangFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
